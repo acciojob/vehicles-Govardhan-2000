43d276e31ac00b457fe870c42d5364d6c819b25b
Govardhan <kurubagovardhan32@gmail.com>
2023-07-17 12:03:12 +0530

    neww code

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -7,7 +7,7 @@ public class Vehicle {
     private int currentDirection;
 
     public Vehicle(String name) {
-gi
+
         this.name = name;
         this.currentDirection = 0;
         this.currentSpeed = 0;
